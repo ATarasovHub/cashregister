4b0673c4b663057d252965bd11c4167f6d34abc5
dhmir <dhmir@tyntec.com>
2025-03-21 14:31:44 +0100

    add ReceiptControllerTest

--- src/main/java/com/pos/cashregister/CashRegisterApplication.java
+++ src/main/java/com/pos/cashregister/CashRegisterApplication.java
@@ -2,9 +2,6 @@ package com.pos.cashregister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CashRegisterApplication {
@@ -13,17 +10,4 @@ public class CashRegisterApplication {
         SpringApplication.run(CashRegisterApplication.class, args);
     }
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:5000", "http://localhost:8080", "https://*.repl.co")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
 }
